52b533c7a81dcfbb54fe9c5bf75fc82fecef3d6c
iwasifirshad <36418003+iwasifirshad@users.noreply.github.com>
2021-07-23 02:25:47 +0530

    Find Value equal to index value

---
+++ Arrays/IndexValue.cpp
@@ -0,0 +1,52 @@
+//Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value.
+
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    // code here
+	    vector<int> res;
+	    int i,j;
+	    for(i=0;i<n;i++)
+	    {
+	        if(arr[i]==i+1)
+	        {
+	            res.push_back(i+1);
+	        }
+	    }
+	    	return res;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.valueEqualToIndex(arr, n);
+        if (ans.empty()) {
+            cout << "Not Found";
+        } else {
+            for (int x: ans) {
+                cout << x << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
